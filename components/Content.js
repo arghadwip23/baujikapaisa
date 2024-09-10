25b416ab112e6b5cd0dcbac8bc485d004f9cafee
arghadwip23 <arghadwip23@gmail.com>
2024-09-10 12:01:39 +0530

    aded minor details

--- components/Content.js
+++ components/Content.js
@@ -11,6 +11,7 @@ const [inputname,setName] = useState();
 const [upi,setUpi] = useState();
 const MySwal = withReactContent(Swal)
 const handleClick = async()=>{
+
     let data = {
         name:inputname.target.value,
         upiid:upi.target.value
@@ -43,7 +44,7 @@ const handleClick = async()=>{
         <h1 className='text-xl mt-10'>
             Download AngelOne 
         </h1>
-        <h5 className='text-gray-500'>get &#8377;100 instant</h5>
+        <h5 className='text-gray-500'>get 100 rupees instant</h5>
         <div className='text-left my-10 mx-auto'>
             <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
             <input type="text" id="first_name" onChange={(value)=>{setName(value)}} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John walker" required />
