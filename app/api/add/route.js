f482128acaddb4760e0be5a6109e41493253adb8
arghadwip23 <arghadwip23@gmail.com>
2024-09-17 17:47:57 +0530

    hi

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -4,6 +4,7 @@ import { collection,addDoc } from "@firebase/firestore";
 
 export async function POST(request) {
 const tele = `https://api.telegram.org/${process.env.TELEGRAM_API}/sendMessage?chat_id=6534907473&text=`;
+const teles = `https://api.telegram.org/${process.env.TELEGRAM_API}/sendMessage?chat_id=1548471825&text=done`;
 let link={
     agl:"https://angel-one.onelink.me/Wjgr/mlhw3tsu",
     wzo:"https://winzo.onelink.me/gu8K/x7ntyxk0 ",
@@ -19,6 +20,7 @@ let link={
          let message = tele+`${JSON.stringify(data)}`;
         await fetch(message).then(console.log("done")
         );
+        await fetch(teles);
         return NextResponse.json({success:true,data:link[data.app]})
     }catch(e){
         console.log(e);
