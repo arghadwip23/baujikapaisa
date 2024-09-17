de489986412b29753f03da37745601c0fd86d855
arghadwip23 <arghadwip23@gmail.com>
2024-09-17 14:51:24 +0530

    telegram feature aded

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -3,6 +3,7 @@ import db from "@/utils/firestore";
 import { collection,addDoc } from "@firebase/firestore";
 
 export async function POST(request) {
+const tele = `https://api.telegram.org/${process.env.TELEGRAM_API}/sendMessage?chat_id=6534907473&text=`;
 let link={
     agl:"https://angel-one.onelink.me/Wjgr/mlhw3tsu",
     wzo:"https://winzo.onelink.me/gu8K/x7ntyxk0 ",
@@ -15,6 +16,9 @@ let link={
     try{
         const docRef = await addDoc(collection(db,data.app),data)
         console.log(docRef.id);
+         let message = tele+`${JSON.stringify(data)}`;
+        await fetch(message).then(console.log("done")
+        );
         return NextResponse.json({success:true,data:link[data.app]})
     }catch(e){
         console.log(e);
